d3faa1557268c982636f2a855de7d93663d9cf7f
Rundskp <runds@kabelplus.at>
2026-01-15 12:43:17 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // - kein cache.addAll() (weil 1x 404 = INSTALL FAIL)
 // - cached einzeln und überspringt kaputte Requests
 
-const CACHE = "rsvp-cache-v3";
+const CACHE = "rsvp-cache-v4";
 
 // WICHTIG: nur Dateien rein, die es wirklich gibt.
 // Wenn du Pfade änderst, hier anpassen.
